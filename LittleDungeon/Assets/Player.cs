221006e7879a06acc2fb915208ea6f7374caa94d
akbarsyamisid <akbar.syam@isid.co.jp>
2019-08-15 17:39:00 +0900

    First Commit

---
+++ LittleDungeon/Assets/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MovingObject
+{
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Return if it's not players turn
+        if (!GameManager.instance.playersTurn) return;
+
+        int horizontal = (int) Input.GetAxisRaw("Horizontal");
+        int vertical = (int) Input.GetAxisRaw("Vertical");
+
+        // For now no diagonal movement, so when x isn't 0 y is turned to 0
+        if (horizontal != 0)
+        {
+            vertical = 0;
+        }
+
+        // If Input isn't 0, attempt to move
+        if (horizontal != 0 || vertical != 0)
+        {
+            AttemptMove(horizontal, vertical);
+        }
+    }
+
+    protected override void AttemptMove(int xDir, int yDir)
+    {
+        base.AttemptMove(xDir, yDir);
+
+        GameManager.instance.playersTurn = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Will check here when picking up items and collided with other object
+    }
+}
---
+++ LittleDungeon/Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float turnDelay = 0.1f;
+    public static GameManager instance = null;
+    [HideInInspector] public bool playersTurn = true;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        } else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playersTurn)
+            return;
+
+        StartCoroutine(MoveEnemies());
+    }
+
+    IEnumerator MoveEnemies()
+    {
+        yield return new WaitForSeconds(turnDelay);
+
+        playersTurn = true;
+    }
+}
